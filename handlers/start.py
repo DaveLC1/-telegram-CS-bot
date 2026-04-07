1a5b8a82f30ea3516198e2e9a0b2ca98c6ed86e6
DaveLC1 <144999652+DaveLC1@users.noreply.github.com>
2026-04-07 16:33:09 +0100

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,7 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from keyboards.courses import get_courses_keyboard
+from db.queries import add_user
 
 
 def main_menu():
@@ -11,15 +12,16 @@ def main_menu():
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    add_user(update.effective_user.id)  # ✅ SAVE USER
     await update.message.reply_text("𝙒𝙚𝙡𝙘𝙤𝙢𝙚 𓉳", reply_markup=main_menu())
 
 
 async def courses_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    add_user(update.effective_user.id)  # ✅ SAVE USER
     await update.message.reply_text(
         "ད𝙎𝙚𝙡𝙚𝙘𝙩 𝙖 𝙘𝙤𝙪𝙧𝙨𝙚:ད",
         reply_markup=get_courses_keyboard()
     )
-
 
 async def report_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data["state"] = "REPORT"
